b01abccaa30a839b60dce25119a5ca0f0cddad51
Fernando Manuel Avila Cataño <feranimaciones@gmail.com>
2020-06-29 23:23:37 -0600

    carga de datos.

--- angular/sistema/src/app/pages/formulario/formulario.component.ts
+++ angular/sistema/src/app/pages/formulario/formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl,AbstractControl,Validators,FormArray,ValidatorFn,ValidationErrors } from '@angular/forms';
 import { BackendApiService } from "app/services/backend-api.service";
+import {Router, ParamMap ,ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -14,9 +15,28 @@ export class FormularioComponent implements OnInit {
   });
   constructor(
     public api:BackendApiService,
+    private activatedRoute: ActivatedRoute,
+
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      if(!params || Object.keys(params).length === 0){
+
+      }else{
+        console.log(params);
+        this.api.getData("usuario/"+params["id"])
+        .subscribe(
+          data => {
+            this.form.patchValue({
+              idusuario:data.usuario.idusuario,
+              usuario:data.usuario.usuario,
+              contrasena:data.usuario.contrasena,
+            });
+          }
+        );
+      }
+    });
   }
   submit():void{
     this.form.disable();
