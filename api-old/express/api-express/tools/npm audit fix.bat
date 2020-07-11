6df12e39b17d0f06d93ca43770d3c529f2aea04d
Fernando M Avila Cataño <feranimaciones@gmail.com>
2020-07-11 14:45:47 -0600

    act

---
+++ api-old/express/api-express/tools/npm audit fix.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+echo ::                                                       ANGULAR                                                     ::
+echo ::                                               ACTUALIZANDO DEPENDENCIAS                                           ::
+echo ::                                                                                                    Version: 1.0.0 ::
+cd ..
+cmd /c npm audit fix
+PAUSE
